b0333ee48e053821b558c7bb7238d549b9a5c1b0
Hau Le <leephuochau@gmail.com>
2023-09-21 09:53:06 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 const express = require("express");
 const cors = require("cors");
-const contactsRouter = require("./app/routes/contact.route");
+const contactRouter = require("./app/routes/contact.route");
 const ApiError = require("./app/api-error");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts/", contactRouter);
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to contact book application"
+    })
+});
 
 app.use((req, res, next) => {
     return next(new ApiError(404, "Resource not found"));
 });
 
-app.use((err, req, res, next) => {
-    return res.status(err.statusCode || 500).json({
-    message: err.message || "Internal Server Error",
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
     });
 });
 
-app.get("/", (req, res) =>{
-    res.json({message: "Welcom to contact book application"});
-});
-
-module.exports = app;
+module.exports = app
